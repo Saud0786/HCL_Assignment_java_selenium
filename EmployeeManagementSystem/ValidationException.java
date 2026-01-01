eb5f9555371b4539ebb8e3291baa9adfb7a01a2a
Saud Ansari <165356306+Saud0786@users.noreply.github.com>
2026-01-02 00:00:12 +0530

    Employee Management System

---
+++ EmployeeManagementSystem/ValidationException.java
@@ -0,0 +1,8 @@
+package EmployeeManagementSystem;
+
+//Custom Exception for validation
+class ValidationException extends Exception {
+ public ValidationException(String message) {
+     super(message);
+ }
+}
