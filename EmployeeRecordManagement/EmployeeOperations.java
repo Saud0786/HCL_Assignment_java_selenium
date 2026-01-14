437e2fb14f987c824884d061b67bce1690dc586e
Saud Ansari <165356306+Saud0786@users.noreply.github.com>
2026-01-15 01:40:16 +0530

    Delete EmployeeRecordManagement directory

--- EmployeeRecordManagement/EmployeeOperations.java
+++
@@ -1,9 +0,0 @@
-package EmployeeRecordManagement;
-
-//Interface defining employee operations
-public interface EmployeeOperations {
- void addEmployee(Employee e);
- void displayEmployees();
- void searchEmployee(int empId);
- void removeEmployee(int empId);
-}
