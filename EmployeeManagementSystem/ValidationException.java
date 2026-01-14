65bf8e6b96b30c37d6cd2ce3c56ed7c5b03c0729
Saud Ansari <165356306+Saud0786@users.noreply.github.com>
2026-01-15 01:39:05 +0530

    Delete EmployeeManagementSystem directory

--- EmployeeManagementSystem/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package EmployeeManagementSystem;
-
-//Custom Exception for validation
-class ValidationException extends Exception {
- public ValidationException(String message) {
-     super(message);
- }
-}
